ec8881b2d924ccf7ee6b3d85abe61a665b0ef62b
nhoktri55 <chanhtri321@gmail.com>
2026-05-21 09:45:41 +0700

    update login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
-import './App.css'
+// import './App.css'
 
 
 //import components
--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -1,7 +1,21 @@
-const Login = () => {
+import './Login.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faGoogle } from '@fortawesome/free-brands-svg-icons';
+
+const Login = (props) => {
   return (
     <div>
-      <h1>Sign In Page</h1>
+      <div className="login-container">
+        <div className="login-background"></div>
+        <div className="login-content">
+          <h2 className='title'>Sign In</h2>
+          <label>Sign In with Google/Facebook</label>
+          <div className='social-login'>
+            <FontAwesomeIcon icon={faGoogle} size="2x" className='social-icon' />
+            <FontAwesomeIcon icon={faFacebook} size="2x" className='social-icon' />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
