de36d0e8e2cd9c29a5e26340d2bdcaffda2870d4
nhoktri55 <chanhtri321@gmail.com>
2026-05-21 11:12:17 +0700

    update login page

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -3,16 +3,22 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faGoogle } from '@fortawesome/free-brands-svg-icons';
 
 const Login = (props) => {
+
+  const handleLogin = (provider) => {
+    // Implement login logic here, e.g., redirect to OAuth provider
+    alert(`Logging in with ${provider}`);
+  }
+
   return (
     <div>
       <div className="login-container">
         <div className="login-background"></div>
         <div className="login-content flex flex-col">
           <h2 className='title'>Sign In</h2>
-          <label>Sign In with Google/Facebook</label>
+          <label className='login-title'>Sign In with Google/Facebook</label>
           <div className='social-login'>
-            <FontAwesomeIcon icon={faGoogle} size="2x" className='social-icon-gg' />
-            <FontAwesomeIcon icon={faFacebook} size="2x" className='social-icon-fa' />
+            <FontAwesomeIcon icon={faGoogle} size="2x" className='social-icon-gg' onClick={(event) => handleLogin("Google")}/>
+            <FontAwesomeIcon icon={faFacebook} size="2x" className='social-icon-fa' onClick={(event) => handleLogin("Facebook")}/>
           </div>
         </div>
       </div>
