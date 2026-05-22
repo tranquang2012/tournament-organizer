708fb7899a8858dffcf36d6608c08c16645a884c
EztocCoun <vuduy30032004@gmail.com>
2026-05-22 13:45:49 +0700

    upd

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,12 +3,12 @@
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
-export default defineConfig({
-  schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-  },
-  datasource: {
-    url: process.env["DATABASE_URL"],
-  },
-});
+// export default defineConfig({
+//   schema: "prisma/schema.prisma",
+//   migrations: {
+//     path: "prisma/migrations",
+//   },
+//   datasource: {
+//     url: process.env["DATABASE_URL"],
+//   },
+// });
