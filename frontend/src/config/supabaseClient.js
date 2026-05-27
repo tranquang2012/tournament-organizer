d2f9d72515891a3455fc751b2e8a4602ef6c83f2
Aqua <s3976073@rmit.edu.vn>
2026-05-27 11:28:44 +0700

    added docker

--- frontend/src/config/supabaseClient.js
+++ frontend/src/config/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  'https://fcllhdeiknlthwqpafiy.supabase.co',
-  'sb_publishable_RXp-v2R5nmE7SCyygiGHHw_XlWXHcKR'
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
 )
