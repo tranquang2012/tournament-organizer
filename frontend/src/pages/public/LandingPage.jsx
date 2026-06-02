68d69b5640be2e72bcdd3f65cbfaa3adc1120425
nhoktri55 <chanhtri321@gmail.com>
2026-06-02 17:52:06 +0700

    merge

--- frontend/src/pages/public/LandingPage.jsx
+++ frontend/src/pages/public/LandingPage.jsx
@@ -12,6 +12,7 @@ const LandingPage = () => {
     <div className="flex flex-col items-center justify-center min-h-[60vh]">
       <h1 className="text-3xl font-semibold text-slate-800 mb-2">Landing</h1>
       <p className="text-base text-slate-400">Landing page placeholder</p>
+      <div className="banner "></div>
     </div>
   )
 }
