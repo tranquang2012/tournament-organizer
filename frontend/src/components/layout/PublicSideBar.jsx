eed460eb10a55e8f0e0ea46a34fabf114625ce03
nhoktri55 <chanhtri321@gmail.com>
2026-06-22 09:32:52 +0700

    add scorll bar to sidebar

--- frontend/src/components/layout/PublicSideBar.jsx
+++ frontend/src/components/layout/PublicSideBar.jsx
@@ -45,9 +45,9 @@ const PublicSidebar = ({ isOpen, onClose }) => {
                     onClick={onClose}
                 />
             )}
-            <aside className={`fixed top-0 left-0 h-full bg-white z-50 shadow-xl transition-all duration-300 ease-in-out overflow-hidden ${isOpen ? 'w-[400px]' : 'w-0'}`}
+            <aside className={`fixed top-0 left-0 h-full overflow-y-auto bg-white z-50 shadow-xl transition-all duration-300 ease-in-out overflow-hidden ${isOpen ? 'w-[400px]' : 'w-0'}`}
             >
-                <div className='h-[80px] w-full px-5 overflow-hidden flex items-center'>
+                <div className='h-[80px] w-full px-5 flex items-center'>
                     <FontAwesomeIcon icon={faBars} className='text-[30px] mr-[7%] cursor-pointer' onClick={onClose} />
                     <img
                         src={logo}
